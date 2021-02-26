e19a7f665ee854ad81e93ea07efedd010d46feab
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-02-26 10:22:05 +0100

    Add Invoice.cs and OrderItem.cs

---
+++ MbmStore/Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Invoice
+    {
+        private decimal totalPrice;
+
+        public int InvoiceId { get; set; }
+        public decimal TotalPrice { get; }
+        public Customer Customer { get; set; }
+        public List<OrderItem> OrderItems { get; }
+
+        public Invoice(int invoiceId, DateTime orderDate, Customer customer)
+        {
+            InvoiceId = invoiceId;
+            Customer = customer;
+        }
+
+        public void AddOrderItem(Product product, int quantity)
+        {
+
+        }
+    }
+}
---
+++ MbmStore/Models/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public OrderItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+    }
+}
