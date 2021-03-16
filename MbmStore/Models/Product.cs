b7ccff6b3362103756c5bc463549650ffdf71499
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-03-16 12:03:06 +0100

    Create ViewModel - Adding Cart.cs and CartLine
    Creating the viewmodels to support the UI.
    The Cartclass uses the CartLineclass, defined in the same file, to represent a product and the quantity selected by the customer. I defined methods to add an item to the cart, remove a previously added item from the cart, calculate the total cost of the items in the cart, and reset the cart by removing all items. I also provided a property Lines that gives access to the contents of the cart using aList<CartLine>.

--- MbmStore/Models/Product.cs
+++ MbmStore/Models/Product.cs
@@ -7,6 +7,7 @@ namespace MbmStore.Models
 {
     public class Product
     {
+        public int ProductID { get; set; }
         public string Title { get; set; }
         public decimal Price { get; set; }
         public string ImageFileName { get; set; }
---
+++ MbmStore/Models/ViewModel/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public decimal TotalPrice { get; set; }
+        public List<CartLine> LineCollection { get; }
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            // Checking to see if the product is already in the cart
+            CartLine item = lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
+            // Add product or update quantity
+            if (item == null)
+            {
+                lineCollection.Add(new CartLine(item.Product = product, item.Quantity = quantity));
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveItem(Product product)
+        {
+            lineCollection.RemoveAll(item => item.Product.ProductID == product.ProductID);
+        }
+
+        public decimal  ComputeTotalValue() =>
+        
+            lineCollection.Sum(e => e.Quantity * e.Product.Price);
+    
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public CartLine(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
