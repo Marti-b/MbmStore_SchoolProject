c7be725fc8283969ad2c271d0f5a8ae6348ffee5
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-03-26 12:33:16 +0100

    Creating a Storable Cart Class by adding SessionCart.cs

---
+++ MbmStore/Models/ViewModel/SessionCart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MbmStore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace MbmStore.Models.ViewModel
+{
+    // 
+    public class SessionCart : Cart
+    {
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<HttpContextAccessor>()?
+                .HttpContext.Session;
+            SessionCart cart = session?.GetJson<SessionCart>("Cart") ??
+                               new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+        [JsonIgnore] 
+        public ISession Session { get; set; }
+
+        public override void AddItem(Product product, int quantity)
+        {
+            base.AddItem(product, quantity);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveLine(Product product)
+        {
+            base.RemoveLines(product);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+        }
+    }
+}
