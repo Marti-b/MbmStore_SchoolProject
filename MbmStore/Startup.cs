910d0389304ac497c8407ca414908b434e70c689
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-03-29 15:27:58 +0200

    Adding Bootstrap to the project

--- MbmStore/Startup.cs
+++ MbmStore/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MbmStore.Models.ViewModels;
+
 using Microsoft.AspNetCore.Http;
 
 namespace Lesson01
