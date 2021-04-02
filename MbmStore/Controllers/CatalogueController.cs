7993b03ce85d9c939097f52f1704f0428a2ea132
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-04-02 19:50:00 +0200

    Creating ProductListViewModel for the actual View

--- MbmStore/Controllers/CatalogueController.cs
+++ MbmStore/Controllers/CatalogueController.cs
@@ -4,19 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MbmStore.Models;
+using MbmStore.Models.ViewModel;
 
 namespace MbmStore.Controllers
 {
     public class CatalogueController : Controller
     {
         public int PageSize = 4;
+        // GET: Catalogue
         public IActionResult Index(int page = 1)
         {
-
-            return View(Repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+            ProductsListViewModel model = new ProductsListViewModel();
+            model = new ProductsListViewModel
+            {
+                Products = Repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = Repository.Products.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ MbmStore/Models/ViewModel/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models.ViewModel
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
