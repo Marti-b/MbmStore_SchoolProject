0c681491657be250df0db92cba40bb7a473b374b
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-04-07 14:03:43 +0200

    Navigation View Component

---
+++ MbmStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MbmStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+
+    }
+}
