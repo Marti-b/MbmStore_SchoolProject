ee75c10f52c10ed81c396cbbb53a5e4b83ee9f20
Marti-b <46929576+Marti-b@users.noreply.github.com>
2021-04-07 14:10:51 +0200

    Generating Category Lists in the controller to pass it to the View

--- MbmStore/Components/NavigationMenuViewComponent.cs
+++ MbmStore/Components/NavigationMenuViewComponent.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MbmStore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MbmStore.Components
 {
     public class NavigationMenuViewComponent : ViewComponent
     {
-        public string Invoke()
+        public IViewComponentResult Invoke()
         {
-            return "Hello from the Nav View Component";
+            return View(Repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
         }
 
     }
